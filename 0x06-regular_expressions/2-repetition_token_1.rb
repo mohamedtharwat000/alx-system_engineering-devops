67945709420d82423efdc4de37fe771f61edab60
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-04 19:42:15 +0300

    Create 2-repetition_token_1.rb

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# Retrieve the argument passed to the script
+argument = ARGV[0]
+
+# Define the regular expression pattern"
+pattern = /hb?tn/
+
+# Use the scan method to find all occurrences of the pattern and join them
+matched_string = argument.scan(pattern).join
+
+# Print the matched part or an empty string if there's no match
+puts matched_string
