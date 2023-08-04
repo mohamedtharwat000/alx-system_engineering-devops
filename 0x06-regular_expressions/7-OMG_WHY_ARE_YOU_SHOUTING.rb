8f076888da1495a7c65b881a2d6866f67496d6a0
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-04 19:55:07 +0300

    Update 7-OMG_WHY_ARE_YOU_SHOUTING.rb

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -4,7 +4,7 @@
 argument = ARGV[0]
 
 # Define the regular expression pattern"
-pattern = /\A[A-Z]+\z/
+pattern = /[A-Z]*/
 
 # Use the scan method to find all occurrences of the pattern and join them
 matched_string = argument.scan(pattern).join
