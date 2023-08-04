75629581b8f0f3a28ef2ecf9194f0d9b15380313
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-08-04 19:35:26 +0300

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -9,5 +9,5 @@ pattern = /School/
 # Apply the regular expression to the argument
 match = argument.match(pattern)
 
-# Print the matched part or an empty string if there's no match
-puts match ? match[0] : ''
+# Join the matche and print the result
+puts matche.join
