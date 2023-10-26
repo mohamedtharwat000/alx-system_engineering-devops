5cc17b3e65093af37a219a29ebbcc8a6546efc74
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-26 16:22:48 +0300

    Create 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Script that, using REST API, for a given employee ID."""
+
+import sys
+import requests
+
+ID = sys.argv[1]
+
+user_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/users/{ID}', timeout=10)
+user_data = user_response.json()
+
+todos_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/todos?userId={ID}', timeout=10)
+todos_data = todos_response.json()
+
+# Calculate progress
+total_tasks = len(todos_data)
+completed_tasks = len([task for task in todos_data if task.get('completed')])
+
+
+# Display the progress
+print(
+    f"Employee {user_data.get('name')} \
+is done with tasks ({completed_tasks}/{total_tasks}):")
+
+for task in todos_data:
+    if task['completed']:
+        print(f"\t{task.get('title')}")
