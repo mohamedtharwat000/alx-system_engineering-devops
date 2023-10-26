eaa60fdcdd5494e8fdb40b0b6130311ce5ce9ad4
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-10-26 16:39:48 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,31 @@
 import requests
 from sys import argv
 
-ID = argv[1]
+def display_tasks():
+    """fetches data for a given employee ID."""
+    ID = argv[1]
 
-user_response = requests.get(
-    f'https://jsonplaceholder.typicode.com/users/{ID}', timeout=10)
-user_data = user_response.json()
+    user_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{ID}', timeout=10)
+    user_data = user_response.json()
 
-todos_response = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={ID}', timeout=10)
-todos_data = todos_response.json()
+    todos_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={ID}', timeout=10)
+    todos_data = todos_response.json()
 
-# Calculate progress
-total_tasks = len(todos_data)
-completed_tasks = len([task for task in todos_data if task.get('completed')])
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = len([task for task in todos_data if task.get('completed')])
 
 
-# Display the progress
-print(
-    f"Employee {user_data.get('name')} \
+    # Display the progress
+    print(
+        f"Employee {user_data.get('name')} \
 is done with tasks ({completed_tasks}/{total_tasks}):")
 
-for task in todos_data:
-    if task['completed']:
-        print(f"\t{task.get('title')}")
+    for task in todos_data:
+        if task['completed']:
+            print(f"\t{task.get('title')}")
+
+if __name__ == "__main__":
+    display_tasks()
