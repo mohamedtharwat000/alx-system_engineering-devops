6441d2312f92386cd27c21f2bbdb2890ab8e59f8
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-09 16:27:34 +0200

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about"
+
+    response = requests.get(url, allow_redirects=False, timeout=10)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
