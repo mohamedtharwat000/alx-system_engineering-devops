f60fca55ff9cb37c15c7558ef3997d1620a65055
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-09 16:42:24 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
