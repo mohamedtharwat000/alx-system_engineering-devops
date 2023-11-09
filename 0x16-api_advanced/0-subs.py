6692c844b451985ced6f1034be6b4b228ea545f7
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-09 16:37:18 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,8 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API and returns the number of subscribers"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about"
-
-    response = requests.get(url, allow_redirects=False, timeout=10)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, allow_redirects=False)
     if response.status_code == 200:
-        subscribers = response.json().get('data').get('subscribers')
-        return subscribers if subscribers else 0
+        return response.json().get("data").get("subscribers")
     return 0
